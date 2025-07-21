3bd33c9774eb735a8cb077d30d0e804d3a3ccbe1
Deciptikon <deciptikon@mail.ru>
2025-07-22 00:28:30 +0300

    Photoalbum-old

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+const BASE_PATH = '/Photoalbum-old'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/vue-facebook/',
+      path: BASE_PATH + '/',
       name: 'home',
       component: () => import('../views/HomeView.vue'),
     },
     {
-      path: '/vue-facebook/about',
+      path: BASE_PATH + '/about',
       name: 'about',
       component: () => import('../views/AboutView.vue'),
     },
     {
-      path: '/vue-facebook/contact',
+      path: BASE_PATH + '/contact',
       name: 'contact',
       component: () => import('../views/ContactView.vue'),
     },
     {
-      path: '/vue-facebook/photoalbum',
+      path: BASE_PATH + '/photoalbum',
       name: 'photoalbum',
       component: () => import('../views/PhotoalbumView.vue'),
     },
