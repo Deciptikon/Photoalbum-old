64860ae4d02a69205315963f0b376b421f2cf812
Deciptikon <deciptikon@mail.ru>
2025-07-22 04:07:51 +0300

    ...

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
     <h1>Добро пожаловать!</h1>
     <p>Набор фотографий из одного потерянного фотоальбома...</p>
     <router-link :to="{ name: 'photoalbum' }">Фотоальбом</router-link> |
-    <router-link :to="{ name: 'about' }">О нас</router-link> |
+    <router-link :to="{ name: 'about' }">О чём это?</router-link> |
     <router-link :to="{ name: 'contact' }">Контакты</router-link>
   </div>
 </template>
